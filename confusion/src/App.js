ee828b69d58ffec060a9d8bb0a244b2fd55af74b
Shawan Mandal <shawan.sm049@gmail.com>
2021-05-18 20:47:08 +0530

    Completed Week 1 Assignments

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,32 +1,54 @@
+// import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
-import { DISHES } from './shared/dishes';
 import Menu from './components/MenuComponent';
-import { Component } from 'react';
-import { render } from 'react-dom';
+// import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
-  constructor(props)
-  {
+
+  constructor(props) {
     super(props);
 
     this.state = {
-      dishes: DISHES
+      dishes: DISHES  // lifting the state up
     };
   }
-render() {
-  return (
-    <div>
-      <Navbar dark color="primary">
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes={this.state.dishes}/>
-    </div>
-  );
+        <Menu dishes={this.state.dishes}/> 
+      </div>
+    );
+  }
 }
-}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,64 +1,58 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-  CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent';
 
 class Menu extends Component {
+
     constructor(props) {
         super(props);
+        
         this.state = {
-          selectedDish: null
+            selectedDish: null
         }
-        }
+        console.log('Menu Component constructor is invoked !');
+    } 
 
-        onDishSelect(dish) {
-          this.setState({ selectedDish: dish });
-        }
-        renderDish(dish){
-          if (dish != null){
-            return(
-              <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                    </CardBody>
-              </Card>
-            );
-          }
-          else {
-            return(
-              <div></div>
-            );
-          }
-        }
+    // lifecycle method -- this method will test componentDidMount() will be invoked
+    componentDidMount() {
+        console.log('Menu Component componentDidMount is invoked !');
+    }
 
-    render() {
+    onDishselect(dish) {
+        this.setState({ 
+            selectedDish: dish
+        });
+    }
+
+    render () {
+        // map (JS keyword): iterating over every dish in the dishes array here
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick={() => {
-                  this.onDishSelect(dish)
-                }}>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
+                // key: every item require key attribute to specift it.
+                // the key helps React to recognise each one of these elements, uniquely. 
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.onDishselect(dish)}> 
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />                    
+                        <CardImgOverlay>
+                            <CardTitle> {dish.name} </CardTitle>
+                        </CardImgOverlay>    
+                    </Card>
+                </div>    
             );
-        });
+        }); // take the JS objects
 
-        return (
-          <div className="container">
-            <div className="row">
-                  {menu}
+        console.log('Menu Component render is invoked !');
+
+         // return what needs to diaplay on the UI in this conponent. (return view)
+        return ( 
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+                <DishDetail dish = {this.state.selectedDish}></DishDetail>
             </div>
-            <div className="row">
-              {this.renderDish(this.state.selectedDish)}
-            </div>
-          </div>
         );
     }
-  }
+}
 
 export default Menu;
