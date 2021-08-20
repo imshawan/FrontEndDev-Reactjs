74b601e1a7d15d94aad2425bcec1f3b17dcfba09
Shawan Mandal <shawan.sm049@gmail.com>
2021-08-20 16:14:51 +0530

    Header and Footer

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Main from './components/MainComponent';
-// import './App.css';
+import './App.css';
 import { DISHES } from './shared/dishes';
 
 class App extends Component {
